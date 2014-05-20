d9772b8af5ac3a2502a0f2725fa78a60c927761b
SeventhSon <myselph.tom@gmail.com>
2014-05-20 17:15:57 +0200

    Moving code around, trying to get things to compile

--- src/raycasting.cu
+++ src/raycasting.cu
@@ -32,7 +32,71 @@ __device__ TColor make_color(float r, float g, float b, float a) {
 			| ((int) (g * 255.0f) << 8) | ((int) (r * 255.0f) << 0);
 }
 
+////////////////////////////////////////////////////////////////////////////////
+// Global data handlers and parameters
+////////////////////////////////////////////////////////////////////////////////
+//Texture reference and channel descriptor for image texture
+texture<uchar4, 2, cudaReadModeNormalizedFloat> texImage;
+cudaChannelFormatDesc uchar4tex = cudaCreateChannelDesc<uchar4>();
+
+//CUDA array descriptor
+cudaArray *a_Src;
+
+#include "utility.cuh"
+////////////////////////////////////////////////////////////////////////////////
+//Raycasting Classes' functions
+////////////////////////////////////////////////////////////////////////////////
+CUDA_CALLABLE_MEMBER const Vector2 Vector2::operator*(const float &q) const{
+	return (Vector2(this->x * 3, this->y * 3));
+}
+
+CUDA_CALLABLE_MEMBER const Vector2 Vector2::operator+(const Vector2& q) const{
+	return (Vector2(this->x + q.x, this->y + q.y));
+}
+
+CUDA_CALLABLE_MEMBER const Vector2 Vector2::operator-(const Vector2& q) const{
+	return (Vector2(x - q.x, y - q.y));
+}
+
+CUDA_CALLABLE_MEMBER const Vector2 Vector2::direction() const{
+	float length = sqrtf((this->x * this->x) + (this->y * this->y));
+	return Vector2(this->x / length, this->y / length);
+}
+
+CUDA_CALLABLE_MEMBER const Vector3 Vector3::operator*(const float q) const{
+	return (Vector3(this->x * q, this->y * q, this->z * q));
+}
+
+CUDA_CALLABLE_MEMBER const Vector3 Vector3::operator+(const Vector3& q) const{
+	return (Vector3(this->x + q.x, this->y + q.y, this->z + q.z));
+}
+
+CUDA_CALLABLE_MEMBER const Vector3 Vector3::operator-(const Vector3& q) const {
+	return (Vector3(this->x - q.x, this->y - q.y, this->z - q.z));
+}
+
+CUDA_CALLABLE_MEMBER const Vector3 Vector3::direction() const{
+	float length = sqrtf((this->x * this->x) + (this->y * this->y) + (this->z * this->z));
+	return Vector3(this->x / length, this->y / length, this->z / length);
+}
+
+CUDA_CALLABLE_MEMBER const Vector3& Ray::origin() const {
+	return m_origin;
+}
+CUDA_CALLABLE_MEMBER const Vector3& Ray::direction() const {
+	return m_direction;
+}
+
+CUDA_CALLABLE_MEMBER const Vector3& Triangle::vertex(int i) const {
+	return m_vertex[i];
+}
+CUDA_CALLABLE_MEMBER const Vector3& Triangle::normal(int i) const {
+	return m_normal[i];
+}
+
+////////////////////////////////////////////////////////////////////////////////
 //Raycasting device functions
+////////////////////////////////////////////////////////////////////////////////
 __device__ Ray computeEyeRay(float x, float y, int width, int height,
 		const Camera& camera) {
 	const float aspect = float(height) / width;
@@ -54,7 +118,7 @@ __device__ bool sampleRayTriangle(const Scene& scene, int x, int y,
 	}
 	// This intersection is closer than the previous one
 	// Intersection point
-	const Point3& P = R.origin() + R.direction() * d;
+	const Vector3& P = R.origin() + R.direction() * d;
 	// Find the interpolated vertex normal at the intersection
 	const Vector3& n = (T.normal(0) * weight[0] + T.normal(1) * weight[1]
 			+ T.normal(2) * weight[2]).direction();
@@ -85,9 +149,9 @@ __device__ float intersect(const Ray& R, const Triangle& T, float weight[3]) {
 	weight[2] = R.direction().dot(r) / a;
 	weight[0] = 1.0f - (weight[1] + weight[2]);
 	const float dist = e2.dot(r) / a;
-	static const float epsilon = 1e-7f;
+	const float epsilon = 1e-7f;
 
-	static const float epsilon2 = 1e-10;
+	const float epsilon2 = 1e-10;
 
 	if ((a <= epsilon) || (weight[0] < -epsilon2) || (weight[1] < -epsilon2)
 			|| (weight[2] < -epsilon2) || (dist <= 0.0f)) {
@@ -101,20 +165,39 @@ __device__ float intersect(const Ray& R, const Triangle& T, float weight[3]) {
 }
 
 ////////////////////////////////////////////////////////////////////////////////
-// Global data handlers and parameters
-////////////////////////////////////////////////////////////////////////////////
-//Texture reference and channel descriptor for image texture
-texture<uchar4, 2, cudaReadModeNormalizedFloat> texImage;
-cudaChannelFormatDesc uchar4tex = cudaCreateChannelDesc<uchar4>();
-
-//CUDA array descriptor
-cudaArray *a_Src;
-
-////////////////////////////////////////////////////////////////////////////////
 // kernels
 ////////////////////////////////////////////////////////////////////////////////
-#include "raycasting_kernel.cuh"
+__global__ void Clear(TColor *dst, int imageW, int imageH) {
+	const int ix = blockDim.x * blockIdx.x + threadIdx.x;
+	const int iy = blockDim.y * blockIdx.y + threadIdx.y;
 
+	if (ix < imageW && iy < imageH) {
+		dst[imageW * iy + ix] = make_color(0.9, 0.5, 1.0, 1.0);
+	}
+}
+
+__global__ void rayCast(TColor *dst, int imageW, int imageH, const Scene& scene,
+		const Camera& camera) {
+	const int ix = blockDim.x * blockIdx.x + threadIdx.x;
+	const int iy = blockDim.y * blockIdx.y + threadIdx.y;
+
+	Radiance3 L_o;
+	const Ray& R = computeEyeRay(ix + 0.5f, iy + 0.5f, imageW, imageH, camera);
+	float distance = INFINITY;
+	dst[imageW * iy + ix] = make_color(1.0, 1.0, 1.0, 1.0);
+	for (unsigned int t = 0; t < scene.triangleCount; ++t) {
+		const Triangle& T = scene.triangles[t];
+		if (sampleRayTriangle(scene, ix, iy, R, T, L_o, distance)) {
+			if (ix < imageW && iy < imageH) {
+				dst[imageW * iy + ix] = make_color(1.0, 0.5, 0.75, 1.0);
+			}
+		}
+	}
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// CUDA code handles
+////////////////////////////////////////////////////////////////////////////////
 extern "C" cudaError_t CUDA_Bind2TextureArray() {
 	return cudaBindTextureToArray(texImage, a_Src);
 }
@@ -138,3 +221,17 @@ extern "C" cudaError_t CUDA_FreeArray() {
 	return cudaFreeArray(a_Src);
 }
 
+extern "C" void cuda_Clear(TColor *d_dst, int imageW, int imageH) {
+	dim3 threads(BLOCKDIM_X, BLOCKDIM_Y);
+	dim3 grid(iDivUp(imageW, BLOCKDIM_X), iDivUp(imageH, BLOCKDIM_Y));
+
+	Clear<<<grid, threads>>>(d_dst, imageW, imageH);
+}
+
+extern "C" void cuda_rayCasting(TColor *d_dst, int imageW, int imageH,
+		const Scene& scene, const Camera& camera) {
+	dim3 threads(BLOCKDIM_X, BLOCKDIM_Y);
+	dim3 grid(iDivUp(imageW, BLOCKDIM_X), iDivUp(imageH, BLOCKDIM_Y));
+
+	rayCast<<<grid, threads>>>(d_dst, imageW, imageH, scene, camera);
+}
---
+++ src/utility.cuh
@@ -0,0 +1,100 @@
+/*
+ * utility.cuh
+ *
+ *  Created on: May 20, 2014
+ *      Author: guru
+ */
+
+#ifndef UTILITY_CUH_
+#define UTILITY_CUH_
+
+#ifdef __CUDACC__
+#define CUDA_CALLABLE_MEMBER __host__ __device__
+#else
+#define CUDA_CALLABLE_MEMBER
+#endif
+
+//Raycasting Classes
+class Vector3 {
+public:
+	float x, y, z;
+	CUDA_CALLABLE_MEMBER Vector3(float xx, float yy, float zz) :
+			y(yy), x(xx), z(zz) {
+	}
+	CUDA_CALLABLE_MEMBER const Vector3 operator*(const float q) const;
+	CUDA_CALLABLE_MEMBER const Vector3 operator+(const Vector3& q) const;
+	CUDA_CALLABLE_MEMBER const Vector3 operator-(const Vector3& q) const;
+	CUDA_CALLABLE_MEMBER const Vector3 direction() const;
+	CUDA_CALLABLE_MEMBER float dot(const Vector3& q) const;
+	CUDA_CALLABLE_MEMBER const Vector3 cross(const Vector3& q) const;
+};
+
+class Vector2 {
+public:
+	float x, y;
+
+	CUDA_CALLABLE_MEMBER Vector2(float xx, float yy) :
+			y(yy), x(xx) {
+	}
+	CUDA_CALLABLE_MEMBER const Vector2 operator*(const float &q) const;
+	CUDA_CALLABLE_MEMBER const Vector2 operator+(const Vector2& q) const;
+	CUDA_CALLABLE_MEMBER const Vector2 operator-(const Vector2& q) const;
+	CUDA_CALLABLE_MEMBER const Vector2 direction() const;
+};
+
+class Color3 {
+public:
+	float r, g, b;
+};
+
+typedef Color3 Radiance3;
+typedef Color3 Power3;
+
+class Ray {
+private:
+	Vector3 m_origin;
+	Vector3 m_direction;
+public:
+	CUDA_CALLABLE_MEMBER Ray(const Vector3& org, const Vector3& dir) :
+			m_origin(org), m_direction(dir) {
+	}
+	CUDA_CALLABLE_MEMBER const Vector3& origin() const;
+	CUDA_CALLABLE_MEMBER const Vector3& direction() const;
+
+};
+
+class Triangle {
+private:
+	Vector3 m_vertex[3];
+	Vector3 m_normal[3];
+public:
+	CUDA_CALLABLE_MEMBER const Vector3& vertex(int i) const;
+	CUDA_CALLABLE_MEMBER const Vector3& normal(int i) const;
+};
+
+
+class Light {
+public:
+	float3 position;
+	TColor color;
+};
+
+class Camera {
+public:
+	float zNear;
+	float zFar;
+	float fieldOfViewX;
+	CUDA_CALLABLE_MEMBER Camera() :
+			zNear(-0.1f), zFar(-100.0f), fieldOfViewX(PI / 2.0f) {
+	}
+};
+
+class Scene {
+	Triangle* triangles;
+	Light* lights;
+	unsigned int triangleCount;
+	unsigned int lightCount;
+};
+
+
+#endif /* UTILITY_CUH_ */
